99af0ec393ba41bcd6f3ca9d5553a7f3c67dce80
Jeroen <Jeroen@i-connect.online>
2024-05-16 16:32:25 +0200

    added enable gpio for sim7000

--- src/TinyGsmClientSIM7000.h
+++ src/TinyGsmClientSIM7000.h
@@ -498,6 +498,24 @@ class TinyGsmSim7000 : public TinyGsmSim70xx<TinyGsmSim7000>,
      */
   public:
 
+    bool enableGpio(uint8_t gpio, bool enable) {
+      //AT+SGPIO=<operation>,<GPIO>,<function>,<level>
+      // <operation> 0 Set the GPIO function including the GPIO output. 1 Read the GPIO level.
+      // Please note that only when the gpioisset asinput, user can use parameter 1 to read the GPIO level, otherwisethemodule will return "ERROR".
+      //<GPIO> The GPIO you want to be set. (It has relations with the hardware, please refer to the hardware manual)
+      // <function> Only when <operation> is set to 0, this option takes effect. 0 Set the GPIO to input. 1 Set the GPIO to output
+      // <level> 0 GPIO low level 1 GPIO high level
+      if (gpio > 7) {
+        log_w("invalid gpio");
+        return false;
+      }
+      char msg[20] = {};
+      sprintf(msg, "+SGPIO=0,%d,1,%d", gpio, enable);
+      sendAT(GF(msg));
+
+      return waitResponse(10000L, GF("OK")) == 1;
+    }
+
     uint8_t getNetworkSystemMode() {
       sendAT(GF("+CNSMOD?"));
       if (waitResponse(GF(GSM_NL "+CNSMOD:")) != 1) {
